9016553f07f18adb678a61440664394b2e6b6cf7
Pranav A. Kothare <pkothare@easydynamics.com>
2021-05-21 17:43:12 -0400

    Update Admonition.tsx
    Parameterize `icon`, `title` and `description` to display information that has been input from the webpart properties pane.

--- src/webparts/admonition/components/Admonition.tsx
+++ src/webparts/admonition/components/Admonition.tsx
@@ -9,9 +9,9 @@ export default class Admonition extends React.Component<IAdmonitionProps, {}> {
   public render(): React.ReactElement<IAdmonitionProps> {
     return (
       <div className={styles.admonition}>
-        <Icon className={styles.alertIcon} iconName="Info" />
-        <div className={styles.alertTitle}>Important</div>
-        <div className={styles.alertBody}>Trusting the developer certificate is required. This is a one-time process and is only required when you run your first SharePoint Framework project on a new workstation. You don't need to do this for every SharePoint Framework project.<br />If you didn't trust the dev cert, follow the steps outlined on this page: <a href="../../set-up-your-development-environment#trusting-the-self-signed-developer-certificate" data-linktype="relative-path">Set up your development environment: Trusting the self-signed developer certificate</a>.</div>
+        <Icon className={styles.alertIcon} iconName={this.props.icon} />
+        <div className={styles.alertTitle}>{this.props.title}</div>
+        <div className={styles.alertBody} dangerouslySetInnerHTML={{ __html: this.props.description }}></div>
       </div>
     );
   }
